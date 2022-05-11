0c3f177ae02829aabfb62b87e8fc1be51c6015fb
Теодора Николаева Компанска <TNKompanska19@codingburgas.bg>
2022-05-11 16:27:06 +0300

    Delete unnexessary files

--- src/pm.types/main/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "..\pm.types\User.h"
-#include "..\pm.dal\UserStore.h"
-
-int main()
-{
-	pm::dal::UserStore(us);
-}
-
--- src/pm.types/pm.types/pm.types.cpp
+++
@@ -1,9 +0,0 @@
-// pm.types.cpp : Defines the functions for the static library.
-//
-
-#include "pch.h"
-#include "User.h"
-#include <vector>
-
-using namespace std;
-
