ba230dc43f55164c9f630898429de5a4173df121
TNKompanska19 <TNKompanska19@codingburgas.bg>
2022-05-28 22:59:29 +0300

    Add option to choose what new user you want to add

--- main/libs/pm.dal/pm.dal.cpp
+++ main/libs/pm.dal/pm.dal.cpp
@@ -39,7 +39,22 @@ namespace pm::dal
         auto const connstr = NANODBC_TEXT("Driver={ODBC Driver 17 for SQL Server};Server=(local)\\SQLExpress;Database=ProjectManager;Trusted_Connection=yes;"); // an ODBC connection string to your database
         nanodbc::connection conn(connstr);
 
-        auto result = nanodbc::execute(conn, "INSERT INTO Users ([First Name], [Last Name], Age, Email, [Password]) VALUES ('bruh', 'nz', 13, 'tory060606@abv.bg', 'Sth67890')");
+        cout << "Enter name:\n";
+        string firstName;
+        cin >> firstName;
+        cout << "\n Enter last name: \n";
+        string lastName;
+        cin >> lastName;
+        cout << "\n Enter age: \n";
+        int age;
+        cin >> age;
+        cout << "\n Enter email: \n";
+        string email;
+        cin >> email;
+        cout << "\n Enter pass: \n";
+        string pass;
+        cin >> pass;
+        auto result = nanodbc::execute(conn, "INSERT INTO Users ([First Name], [Last Name], Age, Email, [Password]) VALUES ('" + firstName +"', '" +lastName  + "', " + to_string(age) + ", '" +email + "', '" + pass + "')");
     }
     catch (std::exception& e)
     {
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 int main()
 {
-	pm::consoleApp::mainMenu();
+	pm::dal::sth();
 }
 
